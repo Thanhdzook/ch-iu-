7a3218ef2cf75962a8692168ce8ec521b411b3d4
Thanhdzook <nhohonlohphp@gmal.com>
2022-08-07 02:21:38 +0700

    iu em

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'hi nhô :>',
+    introDesc: `em mới tạo được cái này hay cực :>`,
+    btnIntro: 'OK',
+    title: 'Đồng chí trả lời cho tôi biết là đồng chí có yêu tôi không ?',
+    desc: 'Không trả lời làm chó :))',
+    btnYes: 'Có , yêu Thành đz vcl lun :>',
+    btnNo: 'Đéo :)) ',
+    question: 'Lí do :))',
+    reasonPlaceholder: 'Trả lời'
+    btnReply: 'Gửi',
+    reply: 'Tại vì em đz đó :>',
+    mess: 'Biết mà ko phải khen :)) cứ khen khen ngta ngại :))',
+    messDesc: 'ấn zo đây để ib cho e :>',
+    btnAccept: 'ấn zo',
+    messLink: 'https://www.facebook.com/thanhdzvclok/'
+}
